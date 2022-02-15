6a672b0d89e0e1042d3da6d35f490a0bbe9d38f2
Cristiano Migali <cristiano.migali@gmail.com>
2022-02-15 16:42:23 +0100

    Added type parameters and comments to collection.g  - Removed dummy.go.

--- collection.go
+++ collection.go
@@ -1,3 +1,7 @@
+/*
+Package livegollection implements a library for live synchronization between backend and frontend of a custom user-implemented collection.
+It's aimed for web applications since it works over websockets.
+*/
 package livegollection
 
 import (
@@ -11,70 +15,95 @@ import (
 	"github.com/m1gwings/livegollection/pool"
 )
 
+// Item is the interface that the items of the user-implemented collection (which is going to be synchronized) must satisfy.
+// It wraps the ID method, which is supposed to return the item's id as a string.
 type Item interface {
 	ID() string
 }
 
-type Collection interface {
-	All() ([]Item, error)
-	Item(ID string) (Item, error)
-	Create(Item) (Item, error)
-	Update(Item) error
+// Collection is the interface that the user-implemented collection (which is going to be synchronized) must satisfy.
+// It wraps the following methods: All, Item, Create, Update, Delete.
+// All returns all the items inside the collection (as a slice of items) or an error if something goes wrong.
+// Item takes an id and returns the correspondant item or an error if something goes wrong.
+// Create takes an item which will be added to the collection.
+// It returns the newly added item after its id field has been set or an error if something goes wrong.
+// Update takes an item which will be updated inside the collection and returns an error if something goes wrong.
+// Delete takes an id and deletes the correspondant item from the collection. It returns an error if something goes wrong.
+type Collection[T Item] interface {
+	All() ([]T, error)
+	Item(ID string) (T, error)
+	Create(T) (T, error)
+	Update(T) error
 	Delete(ID string) error
 }
 
-type LiveCollection struct {
-	coll   Collection
+// LiveGollection represents the instance of the livegollection server.
+// It handles the pool of clients by sending and receiving live updates and
+// calls the appropriate methods on the underlying user-implemented collection to keep it synchronized.
+type LiveGollection[T Item] struct {
+	coll   Collection[T]
 	pool   *pool.Pool
 	logger *log.Logger
 }
 
-func NewLiveCollection(ctx context.Context, coll Collection, logger *log.Logger) *LiveCollection {
-	lC := &LiveCollection{
+// NewLiveGollection returns a pointer to a new LiveGollection instance after having set it properly.
+// It takes the following parameters: ctx, coll, logger.
+// ctx is the Context object that can be used to terminate the LiveGollection and close the connections with all the clients in the pool.
+// Pass context.TODO() if it's unclear which context to use.
+// coll is the istance of the underlying user-implemented collection that will be synchronized.
+// logger is where all the errors and messages will be reported.
+// Pass nil if logging is not needed.
+func NewLiveGollection[T Item](ctx context.Context, coll Collection[T], logger *log.Logger) *LiveGollection[T] {
+	lG := &LiveGollection[T]{
 		coll:   coll,
 		pool:   pool.NewPool(logger),
 		logger: logger,
 	}
 
-	go lC.updatesHandler(ctx)
+	go lG.updatesHandler(ctx)
 
-	return lC
+	return lG
 }
 
-// TODO: Find a more elegant way.
-func (lC *LiveCollection) logError(err error) {
-	if lC.logger == nil {
+// logError, after having checked that the logger has been set,
+// logs errors, adding to them the special prefix: "livegollection: ".
+func (lG *LiveGollection[T]) logError(err error) {
+	if lG.logger == nil {
 		return
 	}
 
-	lC.logger.Printf("livegollection: %v\n", err)
+	lG.logger.Printf("livegollection: %v\n", err)
 }
 
-func (lC *LiveCollection) Join(w http.ResponseWriter, r *http.Request) {
-	initialItems, err := lC.coll.All()
+// Join is an http.HandlerFunc.
+// The proper way to set the websocket server-side handler of livegollection is to
+// add a route to this function with, for example, http.HandleFunc("route/to/livegollection", liveGoll.Join) .
+func (lG *LiveGollection[T]) Join(w http.ResponseWriter, r *http.Request) {
+	initialItems, err := lG.coll.All()
 	if err != nil {
-		lC.logError(fmt.Errorf("error in Join from coll.GetAll: %v", err))
+		lG.logError(fmt.Errorf("error in Join from coll.GetAll: %v", err))
 	}
 
 	initialMessages := make([][]byte, 0, len(initialItems))
 	for _, item := range initialItems {
-		var updMess updateMess
+		var updMess updateMess[T]
 		updMess.Method = createMethodString
 		updMess.ID = item.ID()
-		updMess.Item = item.(DummyItem)
+		updMess.Item = item
 		message, err := json.Marshal(updMess)
 		if err != nil {
-			lC.logError(err)
+			lG.logError(err)
 			continue
 		}
 		initialMessages = append(initialMessages, message)
 	}
 
-	lC.pool.AddToPool(w, r, websocket.TextMessage, initialMessages)
+	lG.pool.AddToPool(w, r, websocket.TextMessage, initialMessages)
 }
 
 type updateMethod string
 
+// Method strings used in update messages to specify the type of event of the update.
 const (
 	createMethodString updateMethod = "CREATE"
 	updateMethodString updateMethod = "UPDATE"
@@ -85,22 +114,26 @@ func isValidUpdateMethod(method updateMethod) bool {
 	return method == createMethodString || method == updateMethodString || method == deleteMethodString
 }
 
-type updateMess struct {
+// updateMess represents the live update messages exchanged with the clients.
+type updateMess[T Item] struct {
 	Method updateMethod `json:"method"`
 	ID     string       `json:"id,omitempty"`   // CREATE update message hasn't an ID
-	Item   DummyItem    `json:"item,omitempty"` // DELETE update message hasn't an item
+	Item   T            `json:"item,omitempty"` // DELETE update message hasn't an item
 }
 
-func (lC *LiveCollection) updatesHandler(ctx context.Context) {
-	defer lC.pool.CloseAll()
+// updatesHandler listens for incoming update messages from clients.
+// When it receives one it calls the appropriate method on the underlying user-implemented collection
+// and then dispatches the message to all the clients in the pool.
+func (lG *LiveGollection[T]) updatesHandler(ctx context.Context) {
+	defer lG.pool.CloseAll()
 
 	for {
 		select {
-		case updateBody := <-lC.pool.ReadQueue():
-			var updMess updateMess
+		case updateBody := <-lG.pool.ReadQueue():
+			var updMess updateMess[T]
 			err := json.Unmarshal(updateBody, &updMess)
 			if err != nil {
-				lC.logError(err)
+				lG.logError(err)
 				continue
 			}
 			if !isValidUpdateMethod(updMess.Method) {
@@ -109,38 +142,37 @@ func (lC *LiveCollection) updatesHandler(ctx context.Context) {
 
 			switch updMess.Method {
 			case createMethodString:
-				var item Item
-				item, err = lC.coll.Create(updMess.Item)
+				item, err := lG.coll.Create(updMess.Item)
 				if err != nil {
-					lC.logError(err)
+					lG.logError(err)
 					continue
 				}
 
 				updMess.ID = item.ID()
-				updMess.Item = item.(DummyItem)
+				updMess.Item = item
 
 			case updateMethodString:
-				err := lC.coll.Update(updMess.Item)
+				err := lG.coll.Update(updMess.Item)
 				if err != nil {
-					lC.logError(err)
+					lG.logError(err)
 					continue
 				}
 
 			case deleteMethodString:
-				err := lC.coll.Delete(updMess.ID)
+				err := lG.coll.Delete(updMess.ID)
 				if err != nil {
-					lC.logError(err)
+					lG.logError(err)
 					continue
 				}
 			}
 
 			toSendData, err := json.Marshal(updMess)
 			if err != nil {
-				lC.logError(err)
+				lG.logError(err)
 				continue
 			}
 
-			lC.pool.SendMessageToAll(websocket.TextMessage, toSendData)
+			lG.pool.SendMessageToAll(websocket.TextMessage, toSendData)
 
 		case <-ctx.Done():
 			return
