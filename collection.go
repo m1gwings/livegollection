ee0911ba804575512d0917c118f9ccf7872a07e3
Cristiano Migali <cristiano.migali@gmail.com>
2022-02-17 18:09:48 +0100

    Added type parameter IdType for items' id

--- collection.go
+++ collection.go
@@ -17,32 +17,36 @@ import (
 )
 
 // Item is the interface that the items of the user-implemented collection (which is going to be synchronized) must satisfy.
-// It wraps the ID method, which is supposed to return the item's id as a string.
-type Item interface {
-	ID() string
+// It wraps the ID method, which is supposed to return the item's id as an IdType where IdType is a type parameter.
+type Item[IdType any] interface {
+	ID() IdType
 }
 
 // Collection is the interface that the user-implemented collection (which is going to be synchronized) must satisfy.
 // It wraps the following methods: All, Item, Create, Update, Delete.
 // All returns all the items inside the collection (as a slice of items) or an error if something goes wrong.
 // Item takes an id and returns the correspondant item or an error if something goes wrong.
-// Create takes an item which will be added to the collection.
-// It returns the newly added item after its id field has been set or an error if something goes wrong.
+// Create takes an item which will be added to the collection. It returns the newly added item after its id field has been set
+// or an error if something goes wrong.
 // Update takes an item which will be updated inside the collection and returns an error if something goes wrong.
 // Delete takes an id and deletes the correspondant item from the collection. It returns an error if something goes wrong.
-type Collection[T Item] interface {
-	All() ([]T, error)
-	Item(ID string) (T, error)
-	Create(T) (T, error)
-	Update(T) error
-	Delete(ID string) error
+// It takes two type parameters: IdType and ItemType. The first is the type of items' id, the second is the type of items
+// that must satisfy Item[IdType] interface.
+type Collection[IdType any, ItemType Item[IdType]] interface {
+	All() ([]ItemType, error)
+	Item(ID IdType) (ItemType, error)
+	Create(ItemType) (ItemType, error)
+	Update(ItemType) error
+	Delete(ID IdType) error
 }
 
 // LiveGollection represents the instance of the livegollection server.
 // It handles the pool of clients by sending and receiving live updates and
 // calls the appropriate methods on the underlying user-implemented collection to keep it synchronized.
-type LiveGollection[T Item] struct {
-	coll   Collection[T]
+// It takes two type parameters: IdType and ItemType. The first is the type of items' id, the second is the type of items
+// that must satisfy Item[IdType] interface.
+type LiveGollection[IdType any, ItemType Item[IdType]] struct {
+	coll   Collection[IdType, ItemType]
 	pool   *pool.Pool
 	logger *log.Logger
 	mu     sync.Mutex
@@ -55,8 +59,11 @@ type LiveGollection[T Item] struct {
 // coll is the istance of the underlying user-implemented collection that will be synchronized.
 // logger is where all the errors and messages will be reported.
 // Pass nil if logging is not needed.
-func NewLiveGollection[T Item](ctx context.Context, coll Collection[T], logger *log.Logger) *LiveGollection[T] {
-	lG := &LiveGollection[T]{
+// It takes two type parameters: IdType and ItemType. The first is the type of items' id in coll, the second is the type of items in coll
+// that must satisfy Item[IdType] interface.
+func NewLiveGollection[IdType any, ItemType Item[IdType]](ctx context.Context,
+	coll Collection[IdType, ItemType], logger *log.Logger) *LiveGollection[IdType, ItemType] {
+	lG := &LiveGollection[IdType, ItemType]{
 		coll:   coll,
 		pool:   pool.NewPool(logger),
 		logger: logger,
@@ -69,7 +76,7 @@ func NewLiveGollection[T Item](ctx context.Context, coll Collection[T], logger *
 
 // logError, after having checked that the logger has been set,
 // logs errors, adding to them the special prefix: "livegollection: ".
-func (lG *LiveGollection[T]) logError(err error) {
+func (lG *LiveGollection[IdType, ItemType]) logError(err error) {
 	if lG.logger == nil {
 		return
 	}
@@ -80,7 +87,7 @@ func (lG *LiveGollection[T]) logError(err error) {
 // Join is an http.HandlerFunc.
 // The proper way to set the websocket server-side handler of livegollection is to
 // add a route to this function with, for example, http.HandleFunc("route/to/livegollection", liveGoll.Join) .
-func (lG *LiveGollection[T]) Join(w http.ResponseWriter, r *http.Request) {
+func (lG *LiveGollection[IdType, ItemType]) Join(w http.ResponseWriter, r *http.Request) {
 	// We need to lock the mutex because otherwise we would miss the updates received
 	// between the call to All and the call to AddToPool.
 	lG.mu.Lock()
@@ -93,7 +100,7 @@ func (lG *LiveGollection[T]) Join(w http.ResponseWriter, r *http.Request) {
 
 	initialMessages := make([][]byte, 0, len(initialItems))
 	for _, item := range initialItems {
-		var updMess updateMess[T]
+		var updMess updateMess[IdType, ItemType]
 		updMess.Method = createMethodString
 		updMess.ID = item.ID()
 		updMess.Item = item
@@ -122,16 +129,16 @@ func isValidUpdateMethod(method updateMethod) bool {
 }
 
 // updateMess represents the live update messages exchanged with the clients.
-type updateMess[T Item] struct {
+type updateMess[IdType any, ItemType Item[IdType]] struct {
 	Method updateMethod `json:"method"`
-	ID     string       `json:"id,omitempty"`   // CREATE update message hasn't an ID
-	Item   T            `json:"item,omitempty"` // DELETE update message hasn't an item
+	ID     IdType       `json:"id,omitempty"` // CREATE update message hasn't an ID
+	Item   ItemType     `json:"item"`
 }
 
 // updatesHandler listens for incoming update messages from clients.
 // When it receives one it calls the appropriate method on the underlying user-implemented collection
 // and then dispatches the message to all the clients in the pool.
-func (lG *LiveGollection[T]) updatesHandler(ctx context.Context) {
+func (lG *LiveGollection[IdType, ItemType]) updatesHandler(ctx context.Context) {
 	defer lG.pool.CloseAll()
 
 	for {
@@ -141,7 +148,7 @@ func (lG *LiveGollection[T]) updatesHandler(ctx context.Context) {
 			// are adding a client to the pool since the new client wouldn't receive them.
 			lG.mu.Lock()
 
-			var updMess updateMess[T]
+			var updMess updateMess[IdType, ItemType]
 			err := json.Unmarshal(updateBody, &updMess)
 			if err != nil {
 				lG.logError(err)
